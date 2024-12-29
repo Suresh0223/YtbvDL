33a310b74d6d9a480a438d3a38f7d981743cba99
Suresh0223 <42685431+Suresh0223@users.noreply.github.com>
2024-12-28 23:37:11 -0800

    Update YTD.py

--- YTD.py
+++ YTD.py
@@ -2,17 +2,16 @@ import streamlit as st
 import os
 import subprocess
 
-st.title("Youtube downloader")
+st.title("YouTube Video Downloader")
 
-url = st.text_input("Enter YouTube URL: ")
+url = st.text_input("Enter YouTube URL:")
 if st.button("Download"):
-  download_video(url)
-  st.success("Video downloaded successfully")
+    download_video(url)
 
 def download_video(url):
-  try:
-    command = f"yt-dlp -x --audio-format mp3 {url}"
-    os.system(command)
-    st.write(f"Downl;oaded video: {url.split('/')[-1]}.mp3")    
-  except Exception as e:
-    st.error(f"Error downloading video: {e}") 
+    try:
+        command = f"yt-dlp -x --audio-format mp3 {url}"
+        os.system(command)
+        st.write(f"Downloaded video: {url.split('/')[-1]}.mp3")
+    except Exception as e:
+        st.write(f"Error: {e}")
